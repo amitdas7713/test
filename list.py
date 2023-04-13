3e1bb1d8141f2ad710556bf3b6add514ae20a96b
unknown <amitdas7713@gmail.com>
2023-04-13 18:55:12 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,16 @@
+x = 5
+y = 7
+def num():
+    if x>y:
+        print("this is greater than x > y ")
+    elif x==y:
+        print("This is equal")
+
+    elif x<y:
+        print("This x greater than y")
+    else:
+        print("this not find number")
+
+n =num()
+print(n)
+
